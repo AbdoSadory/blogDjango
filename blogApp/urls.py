4c414f1e9c304ae6dfa9c6fc4983485adff50182
Abdelrhman Sayed El-Sadory <abdo_sadory@yahoo.com>
2024-01-31 12:46:56 +0200

    writer apis have been done

--- blogApp/urls.py
+++ blogApp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import Blogs,Blog_id
+from .views import Blogs,Blog_id,Writers,Writer_id
 urlpatterns = [
     path('blogs/',Blogs.as_view()),
     path('blogs/<int:id>',Blog_id.as_view()),
+    path('writers/',Writers.as_view()),
+    path('writers/<int:id>',Writer_id.as_view()),
 ]
--- blogApp/views.py
+++ blogApp/views.py
@@ -1,8 +1,8 @@
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .models import Blog
-from .serializers import BlogSerializer
+from .models import Blog,Writer
+from .serializers import BlogSerializer,WriterSerializer
 
 # Create your views here.
 class Blogs(APIView):
@@ -43,3 +43,44 @@ class Blog_id(APIView):
         blog= self.get_object(id)
         blog.delete()
         return Response({"message":"Deleted successfully"},status=200)
+    
+
+class Writers(APIView):
+    def get(self,req):
+        writers = Writer.objects.values()
+        serializedObject = WriterSerializer(writers,many=True)
+        return Response(serializedObject.data,status=200)
+    def post(self,req):
+        serializer = WriterSerializer(data=req.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=201)
+        return Response(serializer.data,status=400)
+
+
+class Writer_id(APIView):
+    
+    def get_object(self,id):
+        try:
+            return Writer.objects.get(pk=id)
+        except Writer.DoesNotExist:
+            raise Http404
+        
+    def get(self,request,id):
+        writer = self.get_object(id)
+        serializedObject = WriterSerializer(writer)
+        return Response(serializedObject.data,status=200)
+    
+    def put(self,request,id):
+        writer = self.get_object(id)
+        serializer = WriterSerializer(writer,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=200)
+        return Response(serializer.data,status=400)
+
+    def delete(self,request,id):
+        writer= self.get_object(id)
+        writer.delete()
+        return Response({"message":"Deleted successfully"},status=200)
+    
